9012275af702540b073d9830594d13b8592a6135
Thor Galli <gallithor@gmail.com>
2023-09-11 15:27:23 -0300

    :lipstick: Add inter font

--- src/styles/ChakraTheme.tsx
+++ src/styles/ChakraTheme.tsx
@@ -1,4 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export const theme = extendTheme({
 	colors: {
@@ -51,5 +53,12 @@ export const theme = extendTheme({
 			900: "#393939",
 		}
 	},
+	styles: {
+		global: {
+			"html, body": {
+				fontFamily: inter,
+			},
+		}
+	}
 });
 

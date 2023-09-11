3a3bbfe971154a7a100d81547ed4f1328cb4b81b
Thor Galli <gallithor@gmail.com>
2023-09-11 10:54:47 -0300

    :truck: Restructure folders (add src)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
+import { theme } from "../styles/ChakraTheme";
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
